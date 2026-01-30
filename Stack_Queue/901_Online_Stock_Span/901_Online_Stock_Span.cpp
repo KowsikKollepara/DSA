b280b4502f60b9c976c7ed51f0136d2c59e55167
KowsikKollepara <kowsik.kollepara06@gmail.com>
2026-01-30 23:52:55 +0530

    Create 901_Online_Stock_Span.cpp

---
+++ Stack_Queue/901_Online_Stock_Span/901_Online_Stock_Span.cpp
@@ -0,0 +1,23 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>>st;
+    int idx=-1;
+    StockSpanner() {
+        idx=-1;
+    }
+    int next(int price) {
+        idx++;
+        while(!st.empty()&&price>=st.top().first){
+            st.pop();
+        }
+        int ans=idx-(st.empty()?-1:st.top().second);
+        st.push({price,idx});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
