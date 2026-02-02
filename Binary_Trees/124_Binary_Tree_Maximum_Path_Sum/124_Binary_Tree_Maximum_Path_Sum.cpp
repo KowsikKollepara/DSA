c3a6c09e39112aab0b09eb84d824a31e5db133e5
KowsikKollepara <kowsik.kollepara06@gmail.com>
2026-02-02 10:34:17 +0530

    Create 124_Binary_Tree_Maximum_Path_Sum.cpp

---
+++ Binary_Trees/124_Binary_Tree_Maximum_Path_Sum/124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi=INT_MIN;          //SHOULD NOT ASSIGN ZERO HERE.
+        traverse(root,maxi);
+        return maxi;
+    }
+private:
+    int traverse(TreeNode* root,int& maxi){
+        if(root==NULL){
+            return 0;
+        }
+        int left=max(0,traverse(root->left,maxi));     //IMPORTANT AS IT AVOIDS MAXIMUM NEGATIVE SUM 
+        int right=max(0,traverse(root->right,maxi));
+        maxi=max(maxi,root->val+left+right);
+        return root->val+max(left,right);
+    }
+
+};
